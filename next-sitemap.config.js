52b66b19e6a07f0a7b2d39e91319884269fae8ae
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-08-01 14:11:32 -0400

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://onebuffalolabs.com',
+  siteUrl: 'https://onebuffalogames.com',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
